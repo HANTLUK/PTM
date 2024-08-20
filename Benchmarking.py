d6ed234a05b325c9299732d35570674597a0e98c
HANTLUK <lukas.freddy@gmx.de>
2024-08-20 13:41:14 +0200

    Mult,Comm,Anti,Conj and TPD first implementations

--- Benchmarking.py
+++ Benchmarking.py
@@ -6,21 +6,43 @@ import time
 import matplotlib.pyplot as plt
 
 
-from Commutator import PTM_commutator
+from Commutator import PTM_commutator,PTM_anticommutator
+from Multiplication import PTM_multiplication
+from Conjugation import PTM_conjugation
+from PTM_utils import matrix_slice
 import TestMatrices as TM
 
-if __name__ == "__main__":
-	maxQdim = 7
+maps = {"Commutator" : PTM_commutator, "Anticommutator" : PTM_anticommutator, "Multiplication" : PTM_multiplication, "Conjugation" : PTM_conjugation,"Slicing":matrix_slice}
+matrizes = {"Diag":TM.diagRandom,"Dense":TM.denseRandom,"Id":TM.identityMatrix}
+maxQdims = {"Commutator" : 7, "Anticommutator" : 7, "Multiplication" : 7, "Conjugation" : 5, "Slicing": 14}
+
+def benchmark(matrixType,mapType,maxQdim):
+	output = True
 	times = []
 	for qDim in range(1,maxQdim):
-		data = TM.diagRandom(2**qDim)
+		data = matrizes[matrixType](2**qDim)
 		start_time = timeit.default_timer()
-		PTM_commutator(data)
+		maps[mapType](data)
 		times.append(timeit.default_timer() - start_time)
-		print("Dim",qDim,timeit.default_timer() - start_time)
-	
+		if output: print("Dim:",qDim,"\t\t Time:",timeit.default_timer() - start_time)
+	return times
+
+def benchmarkPlot(matrixType,mapType,maxQdim):
+	times = benchmark(matrixType,mapType,maxQdim)
 	fig, ax = plt.subplots()
-	ax.scatter(range(1,maxQdim),times)
+	ax.plot(range(1,maxQdim),times)
 	ax.set_yscale('log')
-	ax.set_title(f"PTM Commutator Diag")
-	plt.savefig(f"PlotsPTM/PTMCommutatorDiag.png",dpi=150)
+	ax.set_xlim([0,maxQdim])
+	ax.set_ylim([10**(-6), 10**2])
+	ax.set_title(f"PTM {mapType} {matrixType}")
+	plt.savefig(f"PlotsPTM/PTM{mapType}{matrixType}.png",dpi=150)
+
+if __name__ == "__main__":
+	matrixTypes = ["Diag","Dense"]
+	mapTypes = ["Commutator","Anticommutator","Multiplication","Conjugation","Slicing"] # 
+	for matrixType in matrixTypes:
+		print("\n"+matrixType)
+		for mapType in mapTypes:
+			maxQdim = maxQdims[mapType]
+			print("\n"+mapType)
+			benchmarkPlot(matrixType,mapType,maxQdim)
--- Commutator.py
+++ Commutator.py
@@ -7,126 +7,89 @@ import scipy.sparse as SS
 
 import TestMatrices as TM
 
-def single_commutator(pauli):
-	if pauli == "I":
-		return []
-	elif pauli == "X":
-		return [[2,3],[3,2]]
-	elif pauli == "Y":
-		return [[3,1],[1,3]]
-	elif pauli == "Z":
-		return [[1,2],[2,1]]
+from PTM_utils import matrix_slice, matrix_embedding
 
-def single_anticommutator(pauli):
-	if pauli == "I":
-		return [[0,0],[1,1],[2,2],[3,3]]
-	elif pauli == "X":
-		return [[0,1],[1,0]]
-	elif pauli == "Y":
-		return [[0,2],[2,0]]
-	elif pauli == "Z":
-		return [[0,3],[3,0]]
+import sys
+np.set_printoptions(suppress=True,linewidth=sys.maxsize,threshold=sys.maxsize)
+
+comX = np.array([[0,0,0,0],[0,0,0,0],[0,0,0,-1j],[0,0,1j,0]])
+comY = np.array([[0,0,0,0],[0,0,0,1j],[0,0,0,0],[0,-1j,0,0]])
+comZ = np.array([[0,0,0,0],[0,0,-1j,0],[0,1j,0,0],[0,0,0,0]])
+
+single_commutator = {"X":comX,"Y":comY,"Z":comZ}
+
+antiI = np.eye(4)
+antiX = np.array([[0,1,0,0],[1,0,0,0],[0,0,0,0],[0,0,0,0]])
+antiY = np.array([[0,0,1,0],[0,0,0,0],[1,0,0,0],[0,0,0,0]])
+antiZ = np.array([[0,0,0,1],[0,0,0,0],[0,0,0,0],[1,0,0,0]])
+single_anticommutator = {"I":antiI,"X":antiX,"Y":antiY,"Z":antiZ}
 
 # Datastructure
-pauliI = ["I",1.,np.add,[[0,0],[1,1]]]
-pauliX = ["X",1.,np.add,[[0,1],[1,0]]]
-pauliY = ["Y",1.j,np.subtract,[[0,1],[1,0]]]
-pauliZ = ["Z",1.,np.subtract,[[0,0],[1,1]]]
-paulis = [pauliI,pauliX,pauliY,pauliZ]
+pauliI = [1.,np.add,[0,3]]
+pauliX = [1.,np.add,[1,2]]
+pauliY = [1.j,np.subtract,[1,2]]
+pauliZ = [1.,np.subtract,[0,3]]
+paulis = {"I":pauliI,"X":pauliX,"Y":pauliY,"Z":pauliZ}
+pauliList = ["I","X","Y","Z"]
 
-def matrix_slice(matrix,coords):
-	debug = False
-	dim = matrix.shape[0]
-	bDim = int(dim/2)
-	if debug: print("Range 1", range(coords[0]*bDim,(coords[0]+1)*bDim))
-	if debug: print("Range 2", range(coords[1]*bDim,(coords[1]+1)*bDim))
-	return matrix[coords[0]*bDim:(coords[0]+1)*bDim,coords[1]*bDim:(coords[1]+1)*bDim]
-
-def matrix_embedding(sparse,coords,eDim):
-	debug = False
-	row,col,data = sparse
-	data = data*len(coords)
-	row_ind = []
-	col_ind = []
-	for coord in coords:
-		row_ind += [rowi + coord[0]*eDim for rowi in row]
-		col_ind += [coli + coord[1]*eDim for coli in col]
-	return [row_ind,col_ind,data]
-
-def PTM_commutator(matrix, factor=1., output="mat"):
+def PTM_commutator(matrix, factor=1.):
 	debug = False
 	"""
 	recursive commutators
 	"""
 	dim = matrix.shape[0]
 	qDim = dim.bit_length() - 1
-	row_ind = []
-	col_ind = []
-	data = []
+	PTMdim = 1 << (2*qDim)
+	PTM = np.zeros((PTMdim,PTMdim),dtype=np.complex64)
 	if qDim == 0:
-		return [[0],[0],[matrix[0][0]*factor]]
+		return np.array([matrix[0][0]*factor])
 	else:
-		for pauli in paulis:
-			rest = pauli[2](matrix_slice(matrix,pauli[3][0]), matrix_slice(matrix,pauli[3][1]))
+		Slices = matrix_slice(matrix)
+		for pauliInd in pauliList:
+			pauli = paulis[pauliInd]
+			rest = pauli[1](Slices[pauli[2][0]], Slices[pauli[2][1]])
 			# Use the tensorproduct commutator rule.
 			if rest.any():
-				if pauli[0] != "I":
-					PTMra = PTM_anticommutator(rest,factor*2.j*pauli[1],None)
-					PTMsc = single_commutator(pauli[0])
-					nrow,ncol,ndata = matrix_embedding(PTMra,PTMsc,4**(qDim-1))
-					row_ind += nrow
-					col_ind += ncol
-					data += ndata
+				if pauliInd != "I":
+					PTMra = PTM_anticommutator(rest,factor*2.j*pauli[0])
+					PTMsc = single_commutator[pauliInd]
+					PTM += np.reshape(np.tensordot(PTMsc,PTMra,axes=0),(PTMdim,PTMdim))
 					del PTMra,PTMsc
 
-				PTMrc = PTM_commutator(rest,factor*2*pauli[1],None)
-				PTMsa = single_anticommutator(pauli[0])
-				nrow,ncol,ndata = matrix_embedding(PTMrc,PTMsa,4**(qDim-1))
-				row_ind += nrow
-				col_ind += ncol
-				data += ndata
+				PTMrc = PTM_commutator(rest,factor*2*pauli[0])
+				PTMsa = single_anticommutator[pauliInd]
+				PTM += np.reshape(np.tensordot(PTMsa,PTMrc,axes=0),(PTMdim,PTMdim))
 				del PTMrc, PTMsa
-	if output == "mat":
-		return SS.csc_array((data, (row_ind, col_ind)), shape=(4**qDim,4**qDim)).toarray()
+	return PTM
 
-	return [row_ind,col_ind,data]
-
-def PTM_anticommutator(matrix, factor=1., output="mat"):
+def PTM_anticommutator(matrix, factor=1.):
 	"""
 	recursive anticommutators
 	"""
 	dim = matrix.shape[0]
 	qDim = dim.bit_length() - 1
-	row_ind = []
-	col_ind = []
-	data = []
+	PTMdim = 1 << (2*qDim)
+	PTM = np.zeros((PTMdim,PTMdim),dtype=np.complex64)
 	if qDim == 0:
-		return [[0],[0],[matrix[0][0]*factor]]
+		return np.array([matrix[0][0]*factor])
 	else:
-		for pauli in paulis:
-			rest = pauli[2](matrix_slice(matrix,pauli[3][0]), matrix_slice(matrix,pauli[3][1]))
+		Slices = matrix_slice(matrix)
+		for pauliInd in pauliList:
+			pauli = paulis[pauliInd]
+			rest = pauli[1](Slices[pauli[2][0]], Slices[pauli[2][1]])
 			# Use the tensorproduct anticommutator rule.
 			if rest.any():
-				PTMra = PTM_anticommutator(rest,factor*2.*pauli[1],None)
-				PTMsa = single_anticommutator(pauli[0])
-				nrow,ncol,ndata = matrix_embedding(PTMra,PTMsa,4**(qDim-1))
-				row_ind += nrow
-				col_ind += ncol
-				data += ndata
+				PTMra = PTM_anticommutator(rest,factor*2.*pauli[0])
+				PTMsa = single_anticommutator[pauliInd]
+				PTM += np.reshape(np.tensordot(PTMsa,PTMra,axes=0),(PTMdim,PTMdim))
 				del PTMra,PTMsa
-
-				PTMrc = PTM_commutator(rest,factor*2.j*pauli[1],None)
-				PTMsc = single_commutator(pauli[0])
-				nrow,ncol,ndata = matrix_embedding(PTMrc,PTMsc,4**(qDim-1))
-				row_ind += nrow
-				col_ind += ncol
-				data += ndata
-				del PTMrc, PTMsc
-
-	if output == "mat":
-		return SS.csc_array((data, (row_ind, col_ind)), shape=(4**qDim,4**qDim)).toarray()
-
-	return [row_ind,col_ind,data]
+				
+				if pauliInd != "I":
+					PTMrc = PTM_commutator(rest,factor*2.j*pauli[0])
+					PTMsc = single_commutator[pauliInd]
+					PTM += np.reshape(np.tensordot(PTMsc,PTMrc,axes=0),(PTMdim,PTMdim))
+					del PTMrc, PTMsc
+	return PTM
 		
 
 if __name__ == "__main__":
