9d8d17870773fcf9254f0270619e0ad6326a8d99
lennartbinkowski <lennart.binkowski@itp.uni-hannover.de>
2024-10-12 11:52:07 +0200

    TPD-based canonical to PTM now faster than Qiskit on 7 qubits

--- Benchmarking.py
+++ Benchmarking.py
@@ -4,102 +4,75 @@ from Commutator import commutator_to_ptm, anticommutator_to_ptm
 from Multiplication import multiplication_to_ptm
 from Conjugation import sandwich_to_ptm
 from Chi import chi_to_ptm
-from SuperOp import can_to_ptm, can_to_ptm_old, iter_can_to_ptm
+from SuperOp import can_to_ptm
 from Choi import choi_to_ptm
 from PTM_utils import matrix_slice
 import test_matrices
 import qiskit.quantum_info.operators.channel as ch  # Choi, Chi, PTM, SuperOp
 
-MAPS = {"Commutator": commutator_to_ptm,
-        "Anticommutator": anticommutator_to_ptm,
-        "Multiplication": multiplication_to_ptm,
-        "Conjugation": sandwich_to_ptm,
-        "Slicing": matrix_slice,
-        "Chi to PTM": chi_to_ptm,
-        "canonical to PTM": can_to_ptm,
-        "canonical to PTM (old)": can_to_ptm_old,
-        "canonical to PTM (iterative)": iter_can_to_ptm,
-        "Choi to PTM": choi_to_ptm}
+MAPS = {"C": ("commutator to PTM", commutator_to_ptm, None, 7),
+        "AC": ("anticommutator to PTM", anticommutator_to_ptm, None, 7),
+        "L": ("left multiplication to PTM", multiplication_to_ptm, None, 7),
+        "M": ("sandwich multiplication to PTM", sandwich_to_ptm, None, 7),
+        "Slicing": ("matrix slicing", matrix_slice, None, 7),
+        "Can": ("canonical to PTM", can_to_ptm, ch.SuperOp, 7),
+        "Chi": ("Chi to PTM", chi_to_ptm, ch.Chi, 7),
+        "Choi": ("Choi to PTM", choi_to_ptm, ch.Choi, 7)
+        }
 
-MAPSQK = {"Chi to PTM": ch.Chi,
-          "Choi to PTM": ch.Choi,
-          "canonical to PTM": ch.SuperOp,
-          "canonical to PTM (parallel)": ch.SuperOp}
-
-MATRICES = {"random diagonal": test_matrices.rand_diag_mat,
-            "random dense": test_matrices.rand_dense_mat,
-            "identity": test_matrices.identity_mat}
-
-MAX_NUM_OF_QUBITS = {"Commutator": 7,
-                     "Anticommutator": 7,
-                     "Multiplication": 7,
-                     "Conjugation": 5,
-                     "Slicing": 14,
-                     "Chi to PTM": 7,
-                     "canonical to PTM": 7,
-                     "canonical to PTM (old)": 7,
-                     "canonical to PTM (iterative)": 7,
-                     "Choi to PTM": 6}
+MATRICES = {"randDiag": ("random diagonal", test_matrices.rand_diag_mat),
+            "randDense": ("random dense", test_matrices.rand_dense_mat),
+            "id": ("identity", test_matrices.identity_mat)
+            }
 
 
-def benchmark(matrix_type, map_type, max_num_of_qubits, SOp=False, QK=False, verbose=False):
+def benchmark(matrix_type: str, map_type: str, from_qiskit=False, verbose=False):
     times = []
-    base = 4 if SOp else 2
-    if QK:
-        print("|   |-> \033[4mQiskit\033[0m")
-    else:
-        print("|   |-> \033[4mTPD-based\033[0m")
+    base = 4 if MAPS[map_type][2] else 2
+    print(f"|   |-> \033[4m{"Qiskit" if from_qiskit else "TPD-based"}\033[0m")
 
-    for num_of_qubits in range(max_num_of_qubits + 1):
-        data = MATRICES[matrix_type](base) if num_of_qubits == 0 else MATRICES[matrix_type](base ** num_of_qubits)
-        if QK:
+    warm_up_data = MATRICES[matrix_type][1](base ** 2)
+    if from_qiskit and MAPS[map_type][2]:
+        warm_up_data = MAPS[map_type][2](warm_up_data)
+        ch.PTM(warm_up_data)
+    else:
+        MAPS[map_type][1](warm_up_data)
+
+    for num_of_qubits in range(1, MAPS[map_type][3] + 1):
+        data = MATRICES[matrix_type][1](base ** num_of_qubits)
+        if from_qiskit and MAPS[map_type][2]:
             start_time = timeit.default_timer()
-            data = MAPSQK[map_type](data)
+            data = MAPS[map_type][2](data)
             ch.PTM(data)
             times.append(timeit.default_timer() - start_time)
         else:
             start_time = timeit.default_timer()
-            MAPS[map_type](data)
+            MAPS[map_type][1](data)
             times.append(timeit.default_timer() - start_time)
         if verbose:
-            if num_of_qubits == 1:
-                print(f"|   |   {num_of_qubits}: {times[-1]}")
-            elif num_of_qubits > 1:
-                print(f"|   |   {num_of_qubits}: {times[-1]} (x{times[-1] / times[-2]})")
+            print(f"|   |   {num_of_qubits}: {times[-1]}{"" if num_of_qubits == 1 else f" (x{times[-1] / times[-2]})"}")
     return times
 
 
-def benchmark_can_to_ptm(matrix_type, SOp=False):
-    benchmark(matrix_type, "canonical to PTM (old)", MAX_NUM_OF_QUBITS["canonical to PTM (old)"], SOp, verbose=True)
-    benchmark(matrix_type, "canonical to PTM (iterative)", MAX_NUM_OF_QUBITS["canonical to PTM (iterative)"], SOp, verbose=True)
-    benchmark(matrix_type, "canonical to PTM", MAX_NUM_OF_QUBITS["canonical to PTM"], SOp, verbose=True)
-
-def benchmarkPlot(matrix_type, map_type, max_num_of_qubits, SOp=False):
-    # fig, ax = plt.subplots()
-    # ax.set_ylim([10 ** (-6), 10 ** 2])
-    times = benchmark(matrix_type, map_type, max_num_of_qubits, SOp, verbose=True)
-    # ax.scatter(range(2, max_num_of_qubits + 1), times[1:])
-    # ax.set_yscale('log')
-    # ax.set_title(f"PTM {map_type} {matrix_type}")
-    if SOp and map_type in MAPSQK.keys():
-        timesQK = benchmark(matrix_type, map_type, max_num_of_qubits, SOp, True, verbose=True)
-        # ax.scatter(range(2, max_num_of_qubits + 1), timesQK)
-        # ax.set_ylim([10 ** (-6), 10 ** 3])
-    # plt.savefig(f"PlotsPTM/PTM{map_type}{matrix_type}.png", dpi=150)
+def benchmark_plot(matrix_type: str, map_type: str):
+    fig, ax = plt.subplots()
+    ax.set_ylim([10 ** (-6), 10 ** 2])
+    times = benchmark(matrix_type, map_type, verbose=True)
+    ax.scatter(range(1, MAPS[map_type][3] + 1), times)
+    ax.set_yscale('log')
+    ax.set_title(f"{MAPS[map_type][0]}: {MATRICES[matrix_type][0]}")
+    if MAPS[map_type][2]:
+        times_qiskit = benchmark(matrix_type, map_type, True, verbose=True)
+        ax.scatter(range(1, MAPS[map_type][3] + 1), times_qiskit)
+        ax.set_ylim([10 ** (-6), 10 ** 3])
+    plt.savefig(f"PlotsPTM/{map_type}-PTM_{matrix_type}.png", dpi=150)
 
 
 if __name__ == "__main__":
-    matrix_types = ["random dense"]
-    # "random diagonal"
-    # map_types = []
-    #	 ["Commutator","Anticommutator","Multiplication","Conjugation","Slicing"]
-    # SOpmapTypes = ["canonical to PTM (parallel)"]  # SuperOp_new, "Chi"
-    for mat_type in matrix_types:
-        benchmark_can_to_ptm(mat_type, True)
-    #     print(f"\033[4m{mat_type}\033[0m")
-    #     for map_type in map_types:
-    #         print(f"|-> \033[4m{map_type}\033[0m")
-    #         benchmarkPlot(mat_type, map_type, MAX_NUM_OF_QUBITS[map_type])
-    #     for map_type in SOpmapTypes:
-    #         print(f"|-> \033[4m{map_type}\033[0m")
-    #         benchmarkPlot(mat_type, map_type, MAX_NUM_OF_QUBITS[map_type], True)
+    MATRIX_TYPES = ["randDiag", "randDense"]
+    MAP_TYPES = ["Can"]
+    for MAT_TYPE in MATRIX_TYPES:
+        print(f"\033[4m{MATRICES[MAT_TYPE][0]}\033[0m")
+        for MAP_TYPE in MAP_TYPES:
+            print(f"|-> \033[4m{MAPS[MAP_TYPE][0]}\033[0m")
+            benchmark_plot(MAT_TYPE, MAP_TYPE)
--- SuperOp.py
+++ SuperOp.py
@@ -3,33 +3,20 @@ import numba as nb
 from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor
 import test_matrices
 from TPD import tpd, itpd
-from IterTPD import iter_tpd, iter_itpd
 from qiskit.quantum_info.operators.channel import SuperOp, PTM
 import sys
 
 np.set_printoptions(suppress=True, linewidth=sys.maxsize)
 
 
-def iter_can_to_ptm(matrix):
-    num_of_qubits: int = (matrix.shape[0].bit_length() - 1) >> 1
-    intermediate = np.apply_along_axis(iter_tpd, 1, matrix, num_of_qubits)
-    return np.apply_along_axis(iter_itpd, 0, intermediate, num_of_qubits)
-
-
-def can_to_ptm_old(matrix):
-    num_of_qubits: int = (matrix.shape[0].bit_length() - 1) >> 1
-    intermediate = np.apply_along_axis(tpd, 1, matrix, num_of_qubits)
-    return np.apply_along_axis(itpd, 0, intermediate, num_of_qubits)
-
-
 def can_to_ptm(matrix):
     num_of_qubits: int = (matrix.shape[0].bit_length() - 1) >> 1
-    result = apply_along_row_parallel(tpd, matrix, num_of_qubits)
-    return apply_along_row_parallel(itpd, result.T, num_of_qubits).T
-
-
-def func_with_args(row, func, num_of_qubits):
-    return func(row, num_of_qubits)
+    if num_of_qubits < 6:
+        intermediate = np.apply_along_axis(tpd, 1, matrix, num_of_qubits)
+        return np.apply_along_axis(itpd, 0, intermediate, num_of_qubits)
+    else:
+        intermediate = apply_along_row_parallel(tpd, matrix, num_of_qubits)
+        return apply_along_row_parallel(itpd, intermediate.T, num_of_qubits).T
 
 
 def apply_along_row_parallel(func, data, num_of_qubits):
@@ -38,11 +25,15 @@ def apply_along_row_parallel(func, data, num_of_qubits):
     return np.array(results)
 
 
+def func_with_args(row, func, num_of_qubits):
+    return func(row, num_of_qubits)
+
+
 if __name__ == "__main__":
     NUM_OF_QUBITS: int = 2
     DATA = test_matrices.rand_dense_mat(4 ** NUM_OF_QUBITS)
     print(np.array_str(DATA, precision=1))
-    mat = iter_can_to_ptm(DATA)
+    mat = can_to_ptm(DATA)
 
     SOp = SuperOp(DATA)
     mat2 = PTM(SOp).data
